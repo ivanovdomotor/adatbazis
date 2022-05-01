462fa727012a8d088c62cf7e3a904c294f6fbf48
ivanovdomotor <d4rkj4d395id>
2022-05-01 23:00:44 +0200

    dinamikus

--- includes/main.php
+++ includes/main.php
@@ -10,19 +10,27 @@
     <?php
     //id, megnevezes, munkavegzeshelye, ido, munkarend, munkafeladatleiras, kotelvaras, opcelvaras, juttatasok, jelentkezesihatarido, status, cegnev,
     // allashirdetonev, allashirdetotelszam, allashirdetoemail
-    $pelda = [
+    /*$pelda = [
             [1, "Takaritót keresünk!", "6720 Szeged, Minta u. 78/a", 1, "teljes",
                 "A cégünk olyan szakembert keres, aki vállalja irodáink kitakarítását a hét minden napján",
                 "8 általános","releváns gyakorlat",250000, "2022.07.01.",1, "Kalocsai révészek Bt.", "Minta Pista", "06-70-123-4567","minta@minta.hu"],
         [2, "Programozó fejlődő céghez", "5440 Kunszentmárton, Egyutca 2.", 0, "remote",
             "Fiatalos csapatba keresünk senior, legalább 10 év gyakorlattal rendelkező fullstack programozókat",
             "Typescript, Angular, Postgres, Bootstrap",550000, "2022.09.11",1, "Kisstílű Kft.", "Rakomány Ervin", "06-20-432-2345", "aszfalt@szotyolade.com"]
-    ];
+    ];*/
+
+
+    include "gethirdetesek.php";
+    $cegek = [];
+    if(isset($mymap)){
+        $cegek = $mymap;
+    }
+
     echo "
     <div class='container'>
     ";
     $i = 0;
-    foreach($pelda as $row){
+    foreach($cegek as $row){
         if($i % 3 == 0){
             echo"
         <div class='row'>
@@ -31,10 +39,12 @@
 
         echo"
         <div class='col'>
-        <div class='card'>
+        <div class='card' style='width: 18rem;'>
           <div class='card-body'>
-            <h5 class='card-title'>{$pelda[$i][1]}</h5>
-            <p class='card-text'>{$pelda[$i][5]}</p>
+            <img src='../ceglogos/{$cegek[$i]["CEGKEP"]}' class='card-img-top'>
+            <h5 class='card-title'>{$cegek[$i]["MEGNEVEZES"]}</h5>
+            <p class='card-text'>{$cegek[$i]["MUNKAFELADATLEIRAS"]}</p>
+            <p class='card-text'><small class='text-muted'>MÉG SZÖVEG</small></p>
             <a href='#' class='btn btn-primary'>Jelentkezés</a>
             </div>
         </div>
