a93a2ec7c2fbac51a8ea17ee795c259f4bc1e32b
ivanovdomotor <d4rkj4d395id>
2022-05-01 21:02:16 +0200

    osszes_felhasznalo_stat

--- includes/_osszes_felhasznalo.php
+++ includes/_osszes_felhasznalo.php
@@ -1,8 +1,13 @@
 <?php
-$felhasznalok_szama = 1;
+$felhasznalok_szama = "N.A.";
 
 include "oci_conn_start.php";
 
+$sql_text = "SELECT COUNT(*) FROM FELHASZNALO WHERE NOT IS_ADMIN = 1";
+include 'oci_select.php';
+if (isset($mymap[0])) {
+$felhasznalok_szama = $mymap[0]["COUNT(*)"];
+}
 
 echo $felhasznalok_szama;
 ?>
