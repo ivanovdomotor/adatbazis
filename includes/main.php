57fbced4f9b9b50b4d6fce28901fa023636c1905
ivanovdomotor <d4rkj4d395id>
2022-05-02 18:12:43 +0200

    card javitas

--- includes/main.php
+++ includes/main.php
@@ -34,8 +34,16 @@ session_start();
             <img src='../ceglogos/{$cegek[$i]["CEGKEP"]}' class='card-img-top'>
             <h5 class='card-title'>{$cegek[$i]["MEGNEVEZES"]}</h5>
             <p class='card-text'>{$cegek[$i]["MUNKAFELADATLEIRAS"]}</p>
-            <p class='card-text'><small class='text-muted'>MÉG SZÖVEG</small></p>
-            <a href='#' class='btn btn-primary'>Jelentkezés</a>";
+            <p class='card-text'>
+            <img height='14' class='' src='../profilepics/{$cegek[$i]["ALLASHIRDETOKEP"]}' style='border-radius: 50%;' >
+            <small class='text-muted'>{$cegek[$i]["TITULUS"]} {$cegek[$i]["VEZETEKNEV"]} {$cegek[$i]["KERESZTNEV"]}</small>
+            </br>
+            <small class='text-muted'>tel: {$cegek[$i]["TELEFONSZAM"]}</br> <a>email: {$cegek[$i]["EMAIL"]}</a></small>
+            </p>
+            
+            <a href='#' class='btn btn-primary'>Jelentkezés</a>
+            
+            ";
 
         if($_SESSION["isAdmin"] == 1){
             echo "
