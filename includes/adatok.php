5824e138f71bded4bdb966f7a88ec8adee929574
Sárdi András <sardi.andras@gmail.com>
2022-05-06 16:00:03 +0200

    szarakodás

--- includes/adatok.php
+++ includes/adatok.php
@@ -1,7 +1,10 @@
 <?php
 session_start();
+if($_SESSION["isAdmin"] != 1 && $_SESSION["isAllaskereso"] != 1 && $_SESSION["isAllashirdeto"] != 1){
+    header("Location: login.php");
+    exit;
+}
 ?>
-
 <html>
 <head>
     <link href="../css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
@@ -9,5 +12,46 @@ session_start();
 
 <body>
 <?php include 'navbar.php'; ?>
+<br/>
+<?php
+include "getfelhasznalo.php";
+$felhasznalo = [];
+if(isset($mymap)){
+    $felhasznalo = $mymap;
+}
+
+
+$i = 0;
+
+echo"
+<h5>Az adatbázisban szereplő felhasználók:</h5>
+";
+
+foreach($felhasznalo as $row){
+    echo"
+        <div class='felhasznalok'>
+            <h5>{$felhasznalo[$i]["FELHASZNALONEV"]}</h5>
+            <?php if({$felhasznalo[$i]["IS_ADMIN"]} == 1) : ?>
+                <h5> admin</h5>
+            <?php else : ?>
+                <h5> felhasználó</h5>
+            <?php endif; ?>
+      </div>
+        ";
+
+
+    $i++;
+}
+echo "
+    </div>
+    ";
+
+?>
+
+
+
+
 </body>
 </html>
+
+
