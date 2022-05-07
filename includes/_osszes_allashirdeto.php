19b16797a8409cf882264748eab03c7038cff586
Sárdi András <sardi.andras@gmail.com>
2022-05-07 02:15:07 +0200

    statisztika javitas

--- includes/_osszes_allashirdeto.php
+++ includes/_osszes_allashirdeto.php
@@ -3,7 +3,7 @@ $allashirdetok_szama = "N.A.";
 
 include "oci_conn_start.php";
 
-$sql_text = "SELECT COUNT(*) FROM FELHASZNALO WHERE ALLASHIRDETO_ID = 1";
+$sql_text = "SELECT COUNT(*) FROM FELHASZNALO WHERE ALLASHIRDETO_ID > 0";
 include 'oci_select.php';
 if (isset($mymap[0])) {
     $allashirdetok_szama = $mymap[0]["COUNT(*)"];
--- includes/_osszes_allaskereso.php
+++ includes/_osszes_allaskereso.php
@@ -3,7 +3,7 @@ $allaskeresok_szama = "N.A.";
 
 include "oci_conn_start.php";
 
-$sql_text = "SELECT COUNT(*) FROM FELHASZNALO WHERE ALLASKERESO_ID = 1";
+$sql_text = "SELECT COUNT(*) FROM FELHASZNALO WHERE ALLASKERESO_ID > 0";
 include 'oci_select.php';
 if (isset($mymap[0])) {
     $allaskeresok_szama = $mymap[0]["COUNT(*)"];
