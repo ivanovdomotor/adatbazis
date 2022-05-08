40ee0a029c679a4d56589e58770610c1ba56f716
Sárdi András <sardi.andras@gmail.com>
2022-05-08 17:40:31 +0200

    egyetemm

--- includes/statisztika.php
+++ includes/statisztika.php
@@ -37,6 +37,7 @@ if($_SESSION["isAdmin"] != 1 && $_SESSION["isAllaskereso"] != 1 && $_SESSION["is
                     <tr><th>Gyors és precíz softskillt tartalmazó önéletrajzok száma:</th><th><?php include "softskillcv2.php";?></th></tr>
                     <tr><th>Sarki kisboltbéli tapasztalattal rendelkező önéletrajzok száma:</th><th><?php include "kisbolt.php";?></th></tr>
                     <tr><th>Sarki kisboltbéli tapasztalattal rendelkező önéletrajzok száma, akik jó vezetők:</th><th><?php include "kisboltjovezeto.php";?></th></tr>
+                    <tr><th>SZTE-n, a PTE-n vagy a PKE-n végzettek száma:</th><th><?php include "egyetem.php";?></th></tr>
 
 
                     </tbody>
